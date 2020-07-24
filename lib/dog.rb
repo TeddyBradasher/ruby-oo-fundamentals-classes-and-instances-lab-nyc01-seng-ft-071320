70be3c42ba492c762261a1fc75023b60d777d657
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-24 03:29:14 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+    attr_accessor :name
+    def initialize (name)
+        @name = name
+    end
+end
+
+fido = Dog.new(fido)
+snoopy = Dog.new(snoopy)
+lassie = Dog.new(lassie)
+
+
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+    attr_accessor :name
+    def initialize (name)
+        @name = name
+    end
+end
+
+    adele_goldberg = Person.new("adele goldberg")
+    alan_kay = Person.new("alan kay")
